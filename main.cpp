89c810a47eef11abed092d1205ac592e2991ee5f
Philippe Jalaber <pjalaber@gmail.com>
2020-04-08 14:03:51 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <QApplication>
+#include <QCoreApplication>
+#include <QQmlApplicationEngine>
+#include <QIcon>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(":/icon.png"));
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
---
+++ systray.qml
@@ -0,0 +1,29 @@
+import QtQuick 2.12
+import Qt.labs.platform 1.1
+
+SystemTrayIcon
+{
+    id: sysTray
+    tooltip: qsTr("PowerPAD")
+    visible: true
+    icon.mask: true
+    icon.source: "icon.png"
+
+    menu: Menu {
+        MenuItem {
+            text: qsTr("Show")
+            onTriggered: applicationWindow.showApp(geometry)
+        }
+        MenuItem {
+            text: qsTr("Quit")
+            onTriggered: Qt.quit()
+        }
+    }
+
+    onActivated: {
+        if (!applicationWindow.visibility && reason !== SystemTrayIcon.Context)
+        {
+            applicationWindow.showApp(geometry)
+        }
+    }
+}
