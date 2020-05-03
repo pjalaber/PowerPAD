2a2033304b320fdc3b7b6d7d5ef3ddb23f73c2c1
Philippe Jalaber <pjalaber@gmail.com>
2020-05-03 19:24:08 +0200

    Update translation

--- PowerPAD_fr_FR.ts
+++ PowerPAD_fr_FR.ts
@@ -43,70 +43,123 @@
     </message>
 </context>
 <context>
+    <name>OptionsDialog</name>
+    <message>
+        <location filename="OptionsDialog.qml" line="9"/>
+        <source>PowerPAD - options</source>
+        <translation>PowerPAD - options</translation>
+    </message>
+    <message>
+        <location filename="OptionsDialog.qml" line="33"/>
+        <source>Default system</source>
+        <translation>Système par défaut</translation>
+    </message>
+</context>
+<context>
     <name>OptionsItem.ui</name>
     <message>
-        <location filename="OptionsItem.ui.qml" line="23"/>
+        <location filename="OptionsItem.ui.qml" line="27"/>
         <source>General</source>
         <translation>Général</translation>
     </message>
     <message>
-        <location filename="OptionsItem.ui.qml" line="27"/>
+        <location filename="OptionsItem.ui.qml" line="31"/>
         <source>Controller</source>
         <translation>Contrôleur</translation>
     </message>
     <message>
-        <location filename="OptionsItem.ui.qml" line="31"/>
+        <location filename="OptionsItem.ui.qml" line="35"/>
         <source>Mouse</source>
         <translation>Souris</translation>
     </message>
     <message>
-        <location filename="OptionsItem.ui.qml" line="35"/>
-        <location filename="OptionsItem.ui.qml" line="105"/>
+        <location filename="OptionsItem.ui.qml" line="39"/>
+        <location filename="OptionsItem.ui.qml" line="154"/>
         <source>About</source>
         <translation>A propos</translation>
     </message>
     <message>
-        <location filename="OptionsItem.ui.qml" line="52"/>
+        <location filename="OptionsItem.ui.qml" line="56"/>
         <source>Play sounds on disable</source>
         <translation>Sons lors de la désactivation</translation>
     </message>
     <message>
-        <location filename="OptionsItem.ui.qml" line="59"/>
+        <location filename="OptionsItem.ui.qml" line="63"/>
         <source>Run on startup</source>
         <translation>Exécuter au démarrage</translation>
     </message>
     <message>
-        <location filename="OptionsItem.ui.qml" line="74"/>
+        <location filename="OptionsItem.ui.qml" line="78"/>
         <source>Language:</source>
         <translation>Langage:</translation>
     </message>
     <message>
-        <location filename="OptionsItem.ui.qml" line="87"/>
+        <location filename="OptionsItem.ui.qml" line="89"/>
+        <location filename="OptionsItem.ui.qml" line="101"/>
+        <source>0%</source>
+        <translation>0%</translation>
+    </message>
+    <message>
+        <location filename="OptionsItem.ui.qml" line="90"/>
+        <source>Left joystick dead zone:</source>
+        <translation>Zone morte du stick gauche:</translation>
+    </message>
+    <message>
+        <location filename="OptionsItem.ui.qml" line="91"/>
+        <location filename="OptionsItem.ui.qml" line="103"/>
+        <source>100%</source>
+        <translation>100%</translation>
+    </message>
+    <message>
+        <location filename="OptionsItem.ui.qml" line="102"/>
+        <source>Right joystick dead zone:</source>
+        <translation>Zone morte du stick droit:</translation>
+    </message>
+    <message>
+        <location filename="OptionsItem.ui.qml" line="115"/>
+        <location filename="OptionsItem.ui.qml" line="127"/>
+        <location filename="OptionsItem.ui.qml" line="139"/>
         <source>Fast</source>
         <translation>Rapide</translation>
     </message>
     <message>
-        <location filename="OptionsItem.ui.qml" line="88"/>
+        <location filename="OptionsItem.ui.qml" line="116"/>
+        <location filename="OptionsItem.ui.qml" line="140"/>
         <source>Slow</source>
         <translation>Lent</translation>
     </message>
     <message>
-        <location filename="OptionsItem.ui.qml" line="89"/>
+        <location filename="OptionsItem.ui.qml" line="117"/>
         <source>Mouse cursor speed:</source>
         <translation>Vitesse du curseur de la souris:</translation>
     </message>
     <message>
-        <location filename="OptionsItem.ui.qml" line="130"/>
+        <location filename="OptionsItem.ui.qml" line="128"/>
+        <source>Off</source>
+        <translation>Aucune</translation>
+    </message>
+    <message>
+        <location filename="OptionsItem.ui.qml" line="129"/>
+        <source>Mouse cursor acceleration:</source>
+        <translation>Accélération du curseur de la souris:</translation>
+    </message>
+    <message>
+        <location filename="OptionsItem.ui.qml" line="141"/>
+        <source>Scroll speed:</source>
+        <translation>Vitesse de défilement:</translation>
+    </message>
+    <message>
+        <location filename="OptionsItem.ui.qml" line="179"/>
         <source>Text Area</source>
         <translation>Zon de texte</translation>
     </message>
     <message>
-        <location filename="OptionsItem.ui.qml" line="166"/>
+        <location filename="OptionsItem.ui.qml" line="215"/>
         <source>Cancel</source>
         <translation>Annuler</translation>
     </message>
     <message>
-        <location filename="OptionsItem.ui.qml" line="171"/>
+        <location filename="OptionsItem.ui.qml" line="220"/>
         <source>OK</source>
         <translation>OK</translation>
     </message>
@@ -137,32 +190,32 @@
 <context>
     <name>RectangleApp.ui</name>
     <message>
-        <location filename="RectangleApp.ui.qml" line="30"/>
+        <location filename="RectangleApp.ui.qml" line="31"/>
         <source>PowerPAD</source>
         <translation>PowerPAD</translation>
     </message>
     <message>
-        <location filename="RectangleApp.ui.qml" line="43"/>
+        <location filename="RectangleApp.ui.qml" line="44"/>
         <source>Connected</source>
         <translation>Connecté</translation>
     </message>
     <message>
-        <location filename="RectangleApp.ui.qml" line="68"/>
+        <location filename="RectangleApp.ui.qml" line="69"/>
         <source>Enabled</source>
         <translation>Activé</translation>
     </message>
     <message>
-        <location filename="RectangleApp.ui.qml" line="82"/>
+        <location filename="RectangleApp.ui.qml" line="83"/>
         <source>( Back + Start )</source>
         <translation>( Back + Start )</translation>
     </message>
     <message>
-        <location filename="RectangleApp.ui.qml" line="100"/>
+        <location filename="RectangleApp.ui.qml" line="101"/>
         <source>More...</source>
         <translation>Plus...</translation>
     </message>
     <message>
-        <location filename="RectangleApp.ui.qml" line="111"/>
+        <location filename="RectangleApp.ui.qml" line="112"/>
         <source>Show&lt;br&gt;options</source>
         <translation>Afficher&lt;br&gt;les options</translation>
     </message>
@@ -193,14 +246,22 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="main.qml" line="97"/>
-        <source>PowerPAD - options</source>
-        <translation>PowerPAD - options</translation>
+        <location filename="main.qml" line="56"/>
+        <source>Error: xinput library not found !</source>
+        <translation>librarie xinput introuvable</translation>
     </message>
     <message>
-        <location filename="main.qml" line="128"/>
+        <location filename="main.qml" line="58"/>
+        <source>Error: xinput symbol not found !</source>
+        <translation>symbole xinput introuvable</translation>
+    </message>
+    <message>
+        <source>PowerPAD - options</source>
+        <translation type="vanished">PowerPAD - options</translation>
+    </message>
+    <message>
         <source>Default system</source>
-        <translation>Système par défaut</translation>
+        <translation type="vanished">Système par défaut</translation>
     </message>
 </context>
 </TS>
